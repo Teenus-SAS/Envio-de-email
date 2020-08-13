a1cc2649e76c019656427e2532184388ecab82a9
EstebanGomezR <63615382+EstebanGomezR@users.noreply.github.com>
2020-08-13 13:34:35 -0500

    Update EnvioCorreo.php

--- EnvioCorreo.php
+++ EnvioCorreo.php
@@ -1,3 +1,4 @@
+//Librerias necesarias para generar el email
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -5,21 +6,22 @@ require 'Phpmailer/Exception.php';
 require 'Phpmailer/PHPMailer.php';
 require 'Phpmailer/SMTP.php';
 $resultado = $_POST['resultado'];
+//si el numero enviado es menor a 5 realiza los pasos para crear y enviar el email
 if ($resultado<5) {
   $mail = new PHPMailer(true);
 
   try {
-      $mail->SMTPDebug = 0;                      // Enable verbose debug output
-      $mail->isSMTP();                                            // Send using SMTP
-      $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-      $mail->Username   = 'vargasartunduaga8@gmail.com';                     // SMTP username
-      $mail->Password   = 'va0204lf';                               // SMTP password
-      $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-      $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+      $mail->SMTPDebug = 0;                     
+      $mail->isSMTP();                                           
+      $mail->Host       = 'smtp.gmail.com';                    
+      $mail->SMTPAuth   = true;                                
+      $mail->Username   = 'vargasartunduaga8@gmail.com';       
+      $mail->Password   = 'va0204lf';                          
+      $mail->SMTPSecure = 'tls';         
+      $mail->Port       = 25;                                  
       $mail->setFrom('vargasartunduaga8@gmail.com', 'Esteban');
-      $mail->addAddress('vargasartunduaga8@gmail.com');     // Add a recipient
-      $mail->isHTML(true);                                  // Set email format to HTML
+      $mail->addAddress('vargasartunduaga8@gmail.com');    
+      $mail->isHTML(true);                                  
       $mail->Subject = 'Encuesta';
       $mail->Body    = 'Una de las preguntas en el despeje del proceso fue validada y seleccionada incorrectamente';
       $mail->send();
